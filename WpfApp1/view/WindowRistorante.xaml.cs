d2f0650d89a1d3241167199805d9229bd14b372b
Roberto Pisu <roberto.pisu03@gmail.com>
2024-03-08 21:36:04 +0100

    implemented view of 'Ristorante' and 'Spiaggia' and the logic of adding items in a view side

--- WpfApp1/view/WindowRistorante.xaml.cs
+++ WpfApp1/view/WindowRistorante.xaml.cs
@@ -23,5 +23,47 @@ namespace WpfApp1.view
         {
             InitializeComponent();
         }
+
+        private const double MinimumSpacing = 20;
+        private const double EdgeSpacing = 20; // Spazio minimo dai bordi del Canvas
+        private const double TableSize = 50;
+
+        private double currentLeft = EdgeSpacing; // Posizione corrente sull'asse X
+        private double currentTop = EdgeSpacing; // Posizione corrente sull'asse Y
+
+        private void salaCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (currentLeft + TableSize + MinimumSpacing > salaCanvas.ActualWidth - EdgeSpacing ||
+                currentTop + TableSize + MinimumSpacing > salaCanvas.ActualHeight - EdgeSpacing)
+            {
+                _ = MessageBox.Show("Non c'è abbastanza spazio per aggiungere un altro tavolo.", "Spazio esaurito", MessageBoxButton.OK, MessageBoxImage.Error);
+                return; // Esci dal metodo
+            }
+            MessageBoxResult result = MessageBox.Show("Vuoi davvero aggiungere un tavolo?", "Conferma", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            // Verifica della risposta dell'utente
+            if (result == MessageBoxResult.Yes)
+            {
+                CheckBox newCheckBox = new CheckBox();
+                Image newTable = new Image
+                {
+                    Source = new BitmapImage(new Uri("../resources/table_icon.png", UriKind.Relative)),
+                    Width = 50,
+                    Height = 50
+                };
+                newCheckBox.Content = newTable;
+                Canvas.SetLeft(newCheckBox, currentLeft);
+                Canvas.SetTop(newCheckBox, currentTop);
+
+                _ = salaCanvas.Children.Add(newCheckBox);
+
+                currentLeft += TableSize + MinimumSpacing;
+                if (currentLeft + TableSize > salaCanvas.ActualWidth - EdgeSpacing)
+                {
+                    currentLeft = EdgeSpacing;
+                    currentTop += TableSize + MinimumSpacing;
+                }
+            }
+        }
     }
 }
--- WpfApp1/view/WindowSpiaggia.xaml.cs
+++ WpfApp1/view/WindowSpiaggia.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp1.view
 {
@@ -22,6 +14,53 @@ namespace WpfApp1.view
         public WindowSpiaggia()
         {
             InitializeComponent();
+        }
+
+        private const double MinimumSpacing = 17; // Spazio minimo tra gli ombrelloni
+        private const double EdgeSpacing = 20; // Spazio minimo dai bordi del Canvas
+        private const double UmbrellaSize = 50;
+
+        private double currentLeft = EdgeSpacing; // Posizione corrente sull'asse X
+        private double currentTop = EdgeSpacing; // Posizione corrente sull'asse Y
+
+        private void spiaggiaCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (currentLeft + UmbrellaSize + MinimumSpacing > spiaggiaCanvas.ActualWidth - EdgeSpacing ||
+                currentTop + UmbrellaSize + MinimumSpacing > spiaggiaCanvas.ActualHeight - EdgeSpacing)
+            {
+                _ = MessageBox.Show("Non c'è abbastanza spazio per aggiungere un altro ombrellone.", "Spazio esaurito", MessageBoxButton.OK, MessageBoxImage.Error);
+                return; // Esci dal metodo
+            }
+            MessageBoxResult result = MessageBox.Show("Vuoi davvero aggiungere un ombrellone?", "Conferma", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            // Verifica della risposta dell'utente
+            if (result == MessageBoxResult.Yes)
+            {
+                // Crea un nuovo ombrellone (ad esempio, un'ellisse gialla)
+
+                CheckBox newCheckBox = new CheckBox();
+                Image newUmbrella = new Image
+                {
+                    Source = new BitmapImage(new Uri("../resources/umbrella_icon.png", UriKind.Relative)),
+                    Width = 50,
+                    Height = 50
+                };
+                newCheckBox.Content = newUmbrella;
+                // Imposta la posizione dell'ombrello
+                Canvas.SetLeft(newCheckBox, currentLeft);
+                Canvas.SetTop(newCheckBox, currentTop);
+
+                // Aggiungi l'ombrello al Canvas
+                _ = spiaggiaCanvas.Children.Add(newCheckBox);
+
+                // Aggiorna la posizione corrente per il prossimo ombrello
+                currentLeft += UmbrellaSize + MinimumSpacing;
+                if (currentLeft + UmbrellaSize > spiaggiaCanvas.ActualWidth - EdgeSpacing)
+                {
+                    currentLeft = EdgeSpacing;
+                    currentTop += UmbrellaSize + MinimumSpacing;
+                }
+            }
         }
     }
 }
